7328a8d84602f26ef331f24f3c5f159acd85d750
Pavlo Valor <pavlo.lucius.valor@gmail.com>
2025-08-25 17:19:25 +0300

    fix: return something from the root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import { getStoryblokApi } from '@storyblok/react';
 
 export default async function HomePage() {
-  const { data } = await getStoryblokApi().get(`cdn/stories/root`, { 
-    version: process.env.NODE_ENV !== 'production' ? 'draft' : 'published'
-  })
 
-  return null
+  return <div>[under construction]</div>
 }
